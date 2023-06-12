8ae3e1cdc0a167c286e8bb3a47ba3cac866662cb
vrushali <vrushalihirudkar1@gmail.com>
2023-06-12 11:29:15 +0530

    Positive and negative number

---
+++ main.java
@@ -0,0 +1,20 @@
+class main
+{
+	public static void main(String[] args)
+	{
+		int a=8;
+		if(a>0)
+		{
+			System.out.println("Number is positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("Number is negative");
+		}
+		else
+		{
+			System.out.println("zero");
+		}
+			
+	}
+}
