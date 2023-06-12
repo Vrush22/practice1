4c6ede0f472045cf39355e1e9b563cf97a9dd703
vrushali <vrushalihirudkar1@gmail.com>
2023-06-12 21:04:02 +0530

    Greatest number between two number

---
+++ text2.java
@@ -0,0 +1,15 @@
+class Team2
+{
+	public static void main(String[] args)
+	{
+		int a=10, b=5;
+		if(a>b)
+		{
+			System.out.println("The number b is greater");
+		}
+		else
+		{
+			System.out.println("The number a is greater");
+		}
+	}
+}
