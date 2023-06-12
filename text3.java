4450485256b23d46e210a6d5afabb8e505b40ee1
vrushali <vrushalihirudkar1@gmail.com>
2023-06-12 21:15:09 +0530

    greatest number between three number

---
+++ text3.java
@@ -0,0 +1,19 @@
+class Team3
+{
+	public static void main(String[] args)
+	{
+		int a=2 , b=5 , c=8;
+		if(a>b && b>c)
+		{
+			System.out.println("The number a is greater");
+		}
+		else if(b>c && c>a)
+		{
+			System.out.println("The number b is greater");
+		}
+		else
+		{
+			System.out.println("The number c is greater");
+		}
+	}
+}
